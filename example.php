b66d0a52de7651b80bf36012be1031acb2e6de9a
unknown <rakybhasan2018@gmail.com>
2020-02-26 17:27:08 -0800

    first commit

---
+++ example.php
@@ -0,0 +1,24 @@
+<?php
+
+  $x = 18;
+  $y = "male";
+  $z =  "Bangladeshi";
+
+
+ if($x>=18){
+   if($y == "male"){
+    if($z == "Bangladeshi"){
+      echo "you can go";
+    }else{
+      echo "tumi bideshi";
+    }
+   }else {
+     echo "tumi maiya";
+   }
+
+ }else{
+   echo "boyosh hoi nai";
+ }
+
+
+ ?>
---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+
+// $mark = 78;
+
+
+// if ($mark >=81 && $mark <= 100) {
+//   echo "Superb";
+// }
+// elseif ($mark >= 71 && $mark <=80) {
+//   echo "A+";
+// }
+// elseif ($mark >= 61 && $mark <=70) {
+//   echo "A";
+// }
+// elseif ($mark >= 51 && $mark <=60) {
+//   echo "A-";
+// }
+// elseif ($mark >= 41 && $mark <=50) {
+//   echo "B";
+// }
+// else {
+//   echo "Fail";
+// }
+
+// function rakyb($a,$b,$c,$d)
+// {
+//   echo $a."<br>".$b."<br>".$c."<br>".$d;
+// }
+// rakyb("amar","sonar","bangla","changla");
+
+
+
+
+
+
+
+// for($i = 1; $i<=10; $i++){
+//   if($i%2 == 1
+// ){
+//     for($b =1; $b<=10; $b++){
+//       $e = $i * $b;
+//       echo $i . "x" . $b . " = $e <br>";
+//     }
+//   }
+// }
+
+
+
+
+function namota(){
+  for($i = 1; $i<=10; $i++){
+    if($i%2 == 1
+  ){
+      for($b =1; $b<=10; $b++){
+        $e = $i * $b;
+        echo $i . "x" . $b . " = $e <br>";
+      }
+    }
+  }
+}
+namota();
+
+
+
+
+
+ ?>
